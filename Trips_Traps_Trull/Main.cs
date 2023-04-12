718c42f935819cbf6443ad54dc655e2e47d5a7dc
Mihhail Lastovski <lastovskim@gmail.com>
2023-04-13 01:17:37 +0300

    upd

--- Trips_Traps_Trull/Main.cs
+++ Trips_Traps_Trull/Main.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Trips_Traps_Trull
@@ -21,10 +21,9 @@ namespace Trips_Traps_Trull
             };
 
             Label label2 = new Label { Text = "Valige välja suurus:", FontSize = 20, Margin = new Thickness(0, 20, 0, 10) };
-            
+
             stackLayout.Children.Add(label);
             stackLayout.Children.Add(label2);
-
 
             for (int i = 3; i < 7; i++)
             {
@@ -44,8 +43,21 @@ namespace Trips_Traps_Trull
                 stackLayout.Children.Add(button);
             }
 
+            Button themeButton = new Button
+            {
+                Text = "Muuda teemat",
+                FontSize = 20,
+                WidthRequest = 200,
+                HeightRequest = 100,
+                BorderWidth = 2,
+                CornerRadius = 10,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            themeButton.Clicked += ThemeButton_Clicked;
+            stackLayout.Children.Add(themeButton);
 
-           Content = stackLayout;
+            Content = stackLayout;
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -53,7 +65,21 @@ namespace Trips_Traps_Trull
             Button button = (Button)sender;
             await Navigation.PushModalAsync(new TicTacToe(button.TabIndex));
         }
+
+        private void ThemeButton_Clicked(object sender, EventArgs e)
+        {
+            string currentTheme = Preferences.Get("theme", "light");
+            if (currentTheme == "light")
+            {
+                Preferences.Set("theme", "dark");
+            }
+            else
+            {
+                Preferences.Set("theme", "light");
+            }
+        }
+        
     }
 
-    
+
 }
--- Trips_Traps_Trull/TicTacToe.cs
+++ Trips_Traps_Trull/TicTacToe.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Trips_Traps_Trull
@@ -10,16 +10,58 @@ namespace Trips_Traps_Trull
     public class TicTacToe : ContentPage
     {
         List<Image> images = new List<Image>();
-        static  
-        Element[,] elements;
+        static Element[,] elements;
+        Button buttonX;
+        Button buttonO;
+        bool isPlayerX = true;
 
         public TicTacToe(int boardSize)
         {
+            string currentTheme = Preferences.Get("theme", "light");
             int boundsValue = boardSize;
             elements = new Element[boundsValue, boundsValue];
             Grid grid = new Grid();
             TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
             tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
+
+            buttonX = new Button
+            {
+                Text = "X",
+                FontSize = 24,
+                WidthRequest = 100,
+                HeightRequest = 30,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.White,
+                BorderWidth = 2,
+                BorderColor = Color.Black,
+                Margin = 10
+            };
+            buttonX.Clicked += ButtonX_Clicked;
+            buttonO = new Button
+            {
+                Text = "O",
+                FontSize = 24,
+                WidthRequest = 100,
+                HeightRequest = 30,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.White,
+                BorderWidth = 2,
+                BorderColor = Color.Black,
+                Margin = 10
+            };
+            buttonO.Clicked += ButtonO_Clicked;
+            StackLayout buttonStack = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    buttonX,
+                    new Label { Text = "või", FontSize = 24, FontAttributes = FontAttributes.Bold, VerticalTextAlignment = TextAlignment.Center },
+                    buttonO
+                }
+            };
+            grid.Children.Add(buttonStack, 0, boundsValue, boundsValue, boundsValue + 1);
+
             for (int i = 0; i < boundsValue; i++)
             {
 
@@ -27,45 +69,50 @@ namespace Trips_Traps_Trull
                 grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
                 for (int y = 0; y < boundsValue; y++)
                 {
-                    Frame frame = new Frame { BorderColor = Color.Black };
+                    Frame frame = new Frame { BorderColor = Color.Black, Margin = 2 };
                     Image image = new Image();
                     image.TabIndex = 0;
+                    if (currentTheme == "dark")
+                    {
+                        image.BackgroundColor = Color.DarkGray;
+                        frame.BackgroundColor = Color.DarkGray;
+                    }
                     image.GestureRecognizers.Add(tapGestureRecognizer);
                     images.Add(image);
                     frame.Content = image;
                     grid.Children.Add(frame, i, y);
                     elements[i, y] = new Element(y, i, image);
-
                 }
             }
             Content = grid;
         }
 
-        int mode = 0;
+        private void ButtonX_Clicked(object sender, EventArgs e) => isPlayerX = true;
+        private void ButtonO_Clicked(object sender, EventArgs e) => isPlayerX = false;
+
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {           
             Image image = (Image)sender;
-            if (image.TabIndex != 1 && mode == 0)
+            if (isPlayerX && image.TabIndex != 1)
             {
                 image.Source = "x.png";
                 image.TabIndex = 1;
-                mode = 1;
                 for (int y = 0; y < elements.GetLength(0); y++)
                 {
                     for (int x = 0; x < elements.GetLength(0); x++)
                     {
                         if (elements[y, x].image == image)
                         {
-                            elements[y, x].value = 'x';                        
+                            elements[y, x].value = 'x';
                         }
                     }
                 }
+                isPlayerX = false;
             }
-            else if (image.TabIndex != 1 && mode == 1)
+            else if (image.TabIndex != 1 && !isPlayerX)
             {
                 image.Source = "o.png";
                 image.TabIndex = 1;
-                mode = 0;
                 for (int y = 0; y < elements.GetLength(0); y++)
                 {
                     for (int x = 0; x < elements.GetLength(0); x++)
@@ -76,11 +123,12 @@ namespace Trips_Traps_Trull
                         }
                     }
                 }
+                isPlayerX = true;
             }
+            buttonO.IsEnabled = false;
+            buttonX.IsEnabled = false;
             CheckWinner('0');
             CheckWinner('x');
-
-
         }
 
         private async void CheckWinner(char symbol) 
@@ -103,20 +151,60 @@ namespace Trips_Traps_Trull
                 if (vertical.Count(f => (f == symbol)) == length && vertical.Length == length)
                 {
                     await DisplayAlert("Palju õnne!", $"Võitjad: {symbol}", "ok");
+                    var answer = await DisplayAlert("!!!", $"Kas alustada mängu uuesti?", "jah", "ei");
+                    if (answer)
+                    {
+                        ResetGame();
+                    }
                 }
                 else if (horizontal.Count(f => (f == symbol)) == length && horizontal.Length == length)
                 {
                     await DisplayAlert("Palju õnne!", $"Võitjad: {symbol}", "ok");
+                    var answer = await DisplayAlert("!!!", $"Kas alustada mängu uuesti?", "jah", "ei");
+                    if (answer)
+                    {
+                        ResetGame();
+                    }
                 }
                 else if (diagonalLr.Count(f => (f == symbol)) == length && diagonalLr.Length == length)
                 {
                     await DisplayAlert("Palju õnne!", $"Võitjad: {symbol}", "ok");
+                    var answer = await DisplayAlert("!!!", $"Kas alustada mängu uuesti?", "jah", "ei");
+                    if (answer)
+                    {
+                        ResetGame();
+                    }
                 }
                 else if (diagonalRl.Count(f => (f == symbol)) == length && diagonalRl.Length == length)
                 {
                     await DisplayAlert("Palju õnne!", $"Võitjad: {symbol}", "ok");
+                    var answer = await DisplayAlert("!!!", $"Kas alustada mängu uuesti?", "jah", "ei");
+                    if (answer)
+                    {
+                        ResetGame();
+                    }
                 }
             }
+        }
+
+        private void ResetGame()
+        {
+            foreach (var image in images)
+            {
+                image.Source = null;
+                image.TabIndex = 0;
+            }
+
+            for (int y = 0; y < elements.GetLength(0); y++)
+            {
+                for (int x = 0; x < elements.GetLength(0); x++)
+                {
+                    elements[y, x].value = ' ';
+                }
+            }
+            isPlayerX = true;
+            buttonO.IsEnabled = true;
+            buttonX.IsEnabled = true;
         }
 
         public class Element
